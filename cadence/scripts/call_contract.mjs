a709de60bac7edbd33c77adf64d0d288af91c06f
Cosmodude <vladislav.lenskii@gmail.com>
2023-07-15 21:24:51 +0900

    used the tutorial scripts for connection, authorisation and quering

--- cadence/scripts/call_contract.mjs
+++ cadence/scripts/call_contract.mjs
@@ -1,10 +1,14 @@
 import * as fcl from "@onflow/fcl"
-import * as types from "@onflow/types"
-import { config } from "@onflow/fcl"
+import {config} from "@onflow/fcl"
+import { useState, useEffect } from "react"
 
 config({
-    "accessNode.api": "https://rest-testnet.onflow.org"
-  })
+    "accessNode.api": "https://rest-testnet.onflow.org",
+    "discovery.wallet": "https://fcl-discovery.onflow.org/testnet/authn",
+    "discovery.wallet.method": "POP/RPC", // Optional. Available methods are "IFRAME/RPC", "POP/RPC", "TAB/RPC" or "HTTP/POST", defaults to "IFRAME/RPC".
+    "app.detail.title": "ArtWalk"
+})
+
 // Get latest block
 const latestBlock = await fcl
 .send([
@@ -19,35 +23,85 @@ const account = await fcl.account("0x6223108937e32f96");
 // Get account at a specific block height
 //await fcl.send([fcl.getAccount("0x6223108937e32f96"), fcl.atBlockHeight(110617606)]).then(fcl.decode)
 
-// Or, create a custom authorization function
-const authzFn = async (txAccount) => {
-  return {
-    ...txAccount,
-    addr: "0x6223108937e32f96",
-    keyId: 0,
-    signingFunction: async(signable) => {
-      return {
-        addr: "0x6223108937e32f96",
-        keyId: 0,
-        signature
-      }
-    }
+export default function Home() {
+
+  const [user, setUser] = useState({loggedIn: null})
+  const [name, setName] = useState('')
+
+  useEffect(() => fcl.currentUser.subscribe(setUser), [])
+
+  const sendQuery = async () => {
+    const profile = await fcl.query({
+      cadence: `
+        import Profile from 0xProfile
+
+        pub fun main(address: Address): Profile.ReadOnly? {
+          return Profile.read(address)
+        }
+      `,
+      args: (arg, t) => [arg(user.addr, t.Address)]
+    })
+
+    setName(profile?.name ?? 'No Profile')
   }
+
+  // NEW
+  const initAccount = async () => {
+    const transactionId = await fcl.mutate({
+      cadence: `
+        import Profile from 0xProfile
+
+        transaction {
+          prepare(account: AuthAccount) {
+            // Only initialize the account if it hasn't already been initialized
+            if (!Profile.check(account.address)) {
+              // This creates and stores the profile in the user's account
+              account.save(<- Profile.new(), to: Profile.privatePath)
+
+              // This creates the public capability that lets applications read the profile's info
+              account.link<&Profile.Base{Profile.Public}>(Profile.publicPath, target: Profile.privatePath)
+            }
+          }
+        }
+      `,
+      payer: fcl.authz,
+      proposer: fcl.authz,
+      authorizations: [fcl.authz],
+      limit: 50
+    })
+
+    const transaction = await fcl.tx(transactionId).onceSealed()
+    console.log(transaction)
+  }
+
+  const AuthedState = () => {
+    return (
+      <div>
+        <div>Address: {user?.addr ?? "No Address"}</div>
+        <div>Profile Name: {name ?? "--"}</div>
+        <button onClick={sendQuery}>Send Query</button>
+        <button onClick={initAccount}>Init Account</button> {/* NEW */}
+        <button onClick={fcl.unauthenticate}>Log Out</button>
+      </div>
+    )
+  }
+
+  const UnauthenticatedState = () => {
+    return (
+      <div>
+        <button onClick={fcl.logIn}>Log In</button>
+        <button onClick={fcl.signUp}>Sign Up</button>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h1>Flow App</h1>
+      {user.loggedIn
+        ? <AuthedState />
+        : <UnauthenticatedState />
+      }
+    </div>
+  )
 }
-
-
-// authorizer is the sender
-await fcl.mutate({
-  cadence: `
-    transaction {
-      prepare(acct: AuthAccount) {}
-    }
-  `,
-  /*
-  proposer: authzFn,
-  payer: authzFn,
-  authorizations: [authzFn],
-  */
-  authz: fcl.currentUser, // Optional. Will default to currentUser if not provided.
-  limit: 50,
-})
--- connect-wallet/src/App.js
+++ connect-wallet/src/App.js
@@ -13,15 +13,88 @@ config({
 })
 
 function App() {
-  const [services, setServices] = useState([])
-  useEffect(() => fcl.discovery.authn.subscribe(res => setServices(res.results)), [])
+  
+    const [user, setUser] = useState({loggedIn: null})
+    const [name, setName] = useState('')
+  
+    useEffect(() => fcl.currentUser.subscribe(setUser), [])
+  
+    const sendQuery = async () => {
+      const profile = await fcl.query({
+        cadence: `
+          import Profile from 0xProfile
+  
+          pub fun main(address: Address): Profile.ReadOnly? {
+            return Profile.read(address)
+          }
+        `,
+        args: (arg, t) => [arg(user.addr, t.Address)]
+      })
+  
+      setName(profile?.name ?? 'No Profile')
+    }
+  
+    // NEW
+    const initAccount = async () => {
+      const transactionId = await fcl.mutate({
+        cadence: `
+          import Profile from 0xProfile
+  
+          transaction {
+            prepare(account: AuthAccount) {
+              // Only initialize the account if it hasn't already been initialized
+              if (!Profile.check(account.address)) {
+                // This creates and stores the profile in the user's account
+                account.save(<- Profile.new(), to: Profile.privatePath)
+  
+                // This creates the public capability that lets applications read the profile's info
+                account.link<&Profile.Base{Profile.Public}>(Profile.publicPath, target: Profile.privatePath)
+              }
+            }
+          }
+        `,
+        payer: fcl.authz,
+        proposer: fcl.authz,
+        authorizations: [fcl.authz],
+        limit: 50
+      })
+  
+      const transaction = await fcl.tx(transactionId).onceSealed()
+      console.log(transaction)
+    }
+  
+    const AuthedState = () => {
+      return (
+        <div>
+          <div>Address: {user?.addr ?? "No Address"}</div>
+          <div>Profile Name: {name ?? "--"}</div>
+          <button onClick={sendQuery}>Send Query</button>
+          <button onClick={initAccount}>Init Account</button> {/* NEW */}
+          <button onClick={fcl.unauthenticate}>Log Out</button>
+        </div>
+      )
+    }
+  
+    const UnauthenticatedState = () => {
+      return (
+        <div>
+          <button onClick={fcl.logIn}>Log In</button>
+          <button onClick={fcl.signUp}>Sign Up</button>
+        </div>
+      )
+    }
+  
+    return (
+      <div>
+        <h1>Flow App</h1>
+        {user.loggedIn
+          ? <AuthedState />
+          : <UnauthenticatedState />
+        }
+      </div>
+    )
+  }
 
- 
-  return (
-    <div>
-    {services.map(service => <button key={service.provider.address} onClick={() => fcl.authenticate({ service })}>Login with {service.provider.name}</button>)}
-  </div>
-  );
-}
+
 
 export default App;
