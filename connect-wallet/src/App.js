524903bcb0100b236c233d583095cf6de68c7883
Cosmodude <vladislav.lenskii@gmail.com>
2023-07-16 19:29:58 +0900

    managed to send mint transaction

--- connect-wallet/src/App.js
+++ connect-wallet/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import * as fcl from "@onflow/fcl"
 import {config} from "@onflow/fcl"
 import { useState, useEffect } from "react"
+import { MINT_ARTWALK } from "./mint.tx"
 
 config({
     "accessNode.api": "https://rest-testnet.onflow.org",
@@ -16,16 +17,17 @@ function App() {
   
     const [user, setUser] = useState({loggedIn: null})
     const [name, setName] = useState('')
+    const [transactionStatus, setTransactionStatus] = useState(null)
   
     useEffect(() => fcl.currentUser.subscribe(setUser), [])
   
     const sendQuery = async () => {
       const profile = await fcl.query({
         cadence: `
-          import Profile from 0xProfile
+        mport ArtWalk from 0xa963b619092736ed
   
-          pub fun main(address: Address): Profile.ReadOnly? {
-            return Profile.read(address)
+          pub fun main(): Int? {
+            return ArtWalk.totalSupply
           }
         `,
         args: (arg, t) => [arg(user.addr, t.Address)]
@@ -62,15 +64,30 @@ function App() {
       const transaction = await fcl.tx(transactionId).onceSealed()
       console.log(transaction)
     }
-  
+
+    const executeTransaction = async () => {
+      const transactionId = await fcl.mutate({
+        cadence: MINT_ARTWALK,
+        args: (arg, t) => [arg("image", t.String),arg("Square", t.String)],
+        payer: fcl.authz,
+        proposer: fcl.authz,
+        authorizations: [fcl.authz],
+        limit: 50
+      })
+    
+      fcl.tx(transactionId).subscribe(res => setTransactionStatus(res.status))
+    }
+
     const AuthedState = () => {
       return (
         <div>
           <div>Address: {user?.addr ?? "No Address"}</div>
           <div>Profile Name: {name ?? "--"}</div>
+          <div>Transaction Status: {transactionStatus ?? "--"}</div> 
           <button onClick={sendQuery}>Send Query</button>
-          <button onClick={initAccount}>Init Account</button> {/* NEW */}
+          <button onClick={initAccount}>Init Account</button> 
           <button onClick={fcl.unauthenticate}>Log Out</button>
+          <button onClick={executeTransaction}>Mint</button>
         </div>
       )
     }
